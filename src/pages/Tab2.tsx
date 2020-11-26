1854ee6a2bc96638373dc929a0adb5c6b3eb7e3b
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-26 12:20:50 -0600

    added display of photos

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -8,32 +8,40 @@ import { usePhotoGallery } from '../hooks/usePhotoGallery';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  
-  const { takePhoto } = usePhotoGallery();
+
+  const { photos, takePhoto } = usePhotoGallery();
 
   return (
     <IonPage>
-      <IonHeader>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Photo Gallery</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+    <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="large">Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Photo Gallery</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-      <IonContent>
-        <IonFab vertical="bottom" horizontal="center" slot="fixed">
-          <IonFabButton onClick={() => takePhoto()}>
-            <IonIcon icon={camera}></IonIcon>
-          </IonFabButton>
-        </IonFab>
-      </IonContent>
-    </IonPage>  
+      <IonGrid>
+        <IonRow>
+          {photos.map((photo, index) => (
+            <IonCol size="6" key={index}>
+              <IonImg src={photo.webviewPath} />
+            </IonCol>
+          ))}
+        </IonRow>
+      </IonGrid>
+
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+        <IonFabButton onClick={() => takePhoto()}>
+          <IonIcon icon={camera}></IonIcon>
+        </IonFabButton>
+      </IonFab>
+
+     </IonContent>
+  </IonPage>
   );
 };
 
